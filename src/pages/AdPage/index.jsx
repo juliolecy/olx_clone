9bb2f9f383181370e3aafcf8b8f1aad9bc6f4ace
jack <juliocesar_19@hotmail.com.br>
2023-08-18 14:16:24 -0300

    ad page style finished

--- src/pages/AdPage/index.jsx
+++ src/pages/AdPage/index.jsx
@@ -1,30 +1,30 @@
 import React, { useEffect, useState } from 'react'
 import * as k from './styles'
-import {PageContainer} from '../../components/MainComponents'
+import { PageContainer } from '../../components/MainComponents'
 import OlxAPI from '../../helpers/OlxAPI'
 import { Link, useParams } from 'react-router-dom/cjs/react-router-dom.min'
 import { Slide } from 'react-slideshow-image'
 import 'react-slideshow-image/dist/styles.css';
 import AdItem from '../../components/partials/AdItem'
 import { BsChat } from 'react-icons/bs'
-const Page = ()=> {
+const Page = () => {
     const api = OlxAPI()
-    const {id} = useParams()
+    const { id } = useParams()
 
     const [loading, setLoading] = useState(true);
     const [adInfo, setAdInfo] = useState({})
 
-    useEffect(()=>{
-        const getAdInfo = async(hash)=>{
+    useEffect(() => {
+        const getAdInfo = async (hash) => {
             const json = await api.getAd(hash, true)
             setAdInfo(json);
             setLoading(false)
         }
         getAdInfo(id);
         console.log(adInfo)
-    },[])
+    }, [])
 
-    const formatDate = (date) =>{
+    const formatDate = (date) => {
         let cDate = new Date(date);
         let months = ['janeiro', 'fevereiro', 'março', 'abril', 'maio', 'junho', 'julho', 'agosto', 'setembro', 'novembro', 'dezembro'];
         let cDay = cDate.getDate();
@@ -33,147 +33,79 @@ const Page = ()=> {
 
         return `${cDay} de ${months[cMonth]} de ${cYear}`;
     }
-    
-    return (    
-       
-<PageContainer>
-    {adInfo.category &&
-    <k.BreadChumb>
-        Você está aqui:
-        <Link to='/'>Home</Link>
-        /
-        <Link to={`/ads?state=${adInfo.stateName}`}> {adInfo.stateName}</Link>
-        /
-        <Link to={`/ads?state=${adInfo.stateName}&cat=${adInfo.category.slug}`}>{adInfo.category.name}</Link>
-        / {adInfo.title}
-    </k.BreadChumb>
-    }
 
-    <k.Content>
-        <div className="content--left">
-        {adInfo.title && <h2>{adInfo.title}</h2>}
-            <div className="ad--image">
-            {adInfo.images && adInfo.images.length > 0 &&
-                    <Slide>
-                        {adInfo.images.map((img, index)=> 
-                        <div className='each-slide' key={index}>
-                            <img src={img} alt=''/>
-                        </div>)}
-                    </Slide>
-                }
-            </div>
-        </div>
+    return (
 
-        <div className="content--right">
-                <div className="price--container">
-                <svg  width="24" height="64" className="sc-VigVT fyGuQe" viewBox="0 0 24 64"><path fill="#6e0ad6" d="M22.557 0h1.442v64h-1.442a8 8 0 01-6.84-3.851l-14.557-24a8 8 0 010-8.298l14.557-24A8 8 0 0122.557 0z"></path></svg>
-                    <div className="price">
-                    {adInfo.priceNegotiable &&
-                    'Preço Negociável'
-                }
-                {!adInfo.priceNegotiable && adInfo.price &&
-                    <div className='price'>
-                        Preço:
-                        <span>R$ {adInfo.price}</span>
-                        </div>
-                }
+        <PageContainer>
+            {adInfo.category &&
+                <k.BreadChumb>
+                    Você está aqui:
+                    <Link to='/'>Home</Link>
+                    /
+                    <Link to={`/ads?state=${adInfo.stateName}`}> {adInfo.stateName}</Link>
+                    /
+                    <Link to={`/ads?state=${adInfo.stateName}&cat=${adInfo.category.slug}`}>{adInfo.category.name}</Link>
+                    / {adInfo.title}
+                </k.BreadChumb>
+            }
 
+            <k.Content>
+                {/* ESQUERDA */}
+                <div className="content--left">
+                    {adInfo.title && <h2>{adInfo.title}</h2>}
+                    <div className="ad--image">
+                        {adInfo.images && adInfo.images.length > 0 &&
+                            <Slide>
+                                {adInfo.images.map((img, index) =>
+                                    <div className='each-slide' key={index}>
+                                        <img src={img} alt='' />
+                                    </div>)}
+                            </Slide>
+                        }
                     </div>
                 </div>
+                        {/* DIREITA */}
+                <div className="content--right">
+                    <div className="price--container">
+                        <svg width="24" height="64" className="sc-VigVT fyGuQe" viewBox="0 0 24 64"><path fill="#6e0ad6" d="M22.557 0h1.442v64h-1.442a8 8 0 01-6.84-3.851l-14.557-24a8 8 0 010-8.298l14.557-24A8 8 0 0122.557 0z"></path></svg>
+                            {adInfo.priceNegotiable &&
+                        <div className="price">
+                                'Preço Negociável'
+                                </div>
+                            }
+                            {!adInfo.priceNegotiable && adInfo.price &&
+                                <div className='price'>
+                                    Preço:
+                                    <span>R$ {adInfo.price}</span>
+                                </div>
+                            }
 
-                <div className="ad--info">
-                {adInfo.title && 
-                        <h2>{adInfo.title}</h2>
-                    }
-
-                    <div className="buttons">
-                        <button className="buy">
-                            Comprar online
-                        </button>
-
-                        <button className="chat">
-                            <BsChat/> Chat
-                        </button>
                     </div>
 
-                    {adInfo.dateCreated &&
-                        <small>Criado em {formatDate(adInfo.dateCreated)}</small>}
-                </div>
-        </div>
-    </k.Content>
+                    <div className="ad--info">
+                        {adInfo.title &&
+                            <h2>{adInfo.title}</h2>
+                        }
 
-    <k.PageArea>    
-        <div className='leftSide'>
-            <div className='box'>
-                <div className='adImage'>
-                {loading && <k.Fake height={300}/>}
-                {adInfo.images && adInfo.images.length === 0 &&
-                <k.FakeImage>
+                        <div className="buttons">
+                            <button className="buy">
+                                Comprar online
+                            </button>
 
-                </k.FakeImage>
-                }
-                
-                </div>
-
-                <div className='adInfo'>
-                    <div className='adName'> {loading && <k.Fake height={20}/>}
-                    {adInfo.title && 
-                        <h2>{adInfo.title}</h2>
-                    }
-                    {adInfo.dateCreated &&
-                        <small>Criado em {formatDate(adInfo.dateCreated)}</small>}
-                </div>
-
-                <div className='adDescription'> {loading && <k.Fake height={100}/>}
-                    {adInfo.description}
-                    <hr />
-                    {adInfo.views && 
-                    <small>Visualizações: {adInfo.views}</small>}
-                </div>
-                </div>
-            </div>
-        </div>      
-        <div className='rightSide'>
-                <div className='box box--padding'>
-                {loading && <k.Fake height={20}/>}
-                {adInfo.priceNegotiable &&
-                    'Preço Negociável'
-                }
-                {!adInfo.priceNegotiable && adInfo.price &&
-                    <div className='price'>
-                        Preço:
-                        <span>R$ {adInfo.price}</span>
+                            <button className="chat">
+                                <BsChat /> Chat
+                            </button>
                         </div>
-                }
-                </div>
-                {loading && <k.Fake height={20}/>}
-                {adInfo.userInfo && 
-                <>
-                <a href={`mailto:${adInfo.userInfo.email}`} className='contactSellerLink' tanger='_blank'>Fale com o vendedor</a>
-                <div className='box createdBy  box--padding'>
-                    <strong>{adInfo.userInfo.name}</strong>
-                    <small>Email: {adInfo.userInfo.email}</small>
-                    <small>Estado: {adInfo.stateName}</small>
-                </div>
-                </>
-                }
-        </div>    
 
-    </k.PageArea>
+                        {adInfo.dateCreated &&
+                            <small>Criado em {formatDate(adInfo.dateCreated)}</small>}
+                    </div>
+                </div>
+            </k.Content>
 
-    <k.OthersArea>
-        {adInfo.others &&
-        <>
-            <h2>Outras ofertas do vendedor</h2>
-            <div className='list'>
-                {adInfo.others.map((item, index)=>(
-                    <AdItem key={index} data={item}/>
-                    ))}
-            </div>
-        </>}  
-    </k.OthersArea>
-</PageContainer>
-)
+  
+        </PageContainer>
+    )
 }
 
 export default Page;
--- src/pages/AdPage/styles.js
+++ src/pages/AdPage/styles.js
@@ -1,160 +1,5 @@
 import styled from 'styled-components';
 
-export const Fake = styled.div`
-    background-color:#DDD;
-    height:${props=>props.height || 20}px;
-`;
-
-export const PageArea = styled.div`
-display:flex;
-margin-top:20px;
-.box {
-    background-color:#FFF;
-    border-radius:5px;
-    box-shadow:0px 0px 4px #999;
-    margin-bottom:20px;
-}
-.box--padding {
-    padding:10px;
-}
-.leftSide {
-    flex:1;
-    margin-right:20px;
-    .box{
-        display:flex;
-    }
-    .adImage {
-        width:400px;
-        height:500px;
-        margin-right:20px;
-        .each-slide img {
-            display:flex;
-            align-items:center;
-            justify-content:center;
-            background-size:cover;
-            height:320;
-        }
-    }
-    .adInfo {
-         flex:1;
-        .adName{
-            margin-bottom:20px;
-            h2 {
-                margin:0;
-                margin-top:20px;
-            }
-            small {
-                color:#999;
-            }
-        }
-        .adDescription {
-            small {
-                color:#999;
-            }
-        }
-    }
-}
-.rightSide {
-    width:250px; 
-
-    .price span {
-        color:#0000FF;
-        display:block;
-        font-size:27px;
-        font-weight:bold;
-    }
-
-    .contactSellerLink {
-        background-color:#0000FF;
-        color:#FFF;
-        height:30px;
-        border-radius:5px;
-        box-shadow:0px 0px 4px #999;
-        display:flex;
-        justify-content:center;
-        align-items:center;
-        text-decoration:none;
-        margin-bottom:20px;
-    }
-
-    .createdBy small {
-        display:block;
-        color:#999;
-        margin-top:10px; 
-    }
-}
-
-@media(max-width:600px){
-    &{
-        flex-direction: column;
-    }
-    .leftSide{
-        margin:0;
-        .box{
-            flex-direction: column;
-            width:320px;
-            margin:auto;
-        }
-
-        .adInfo{
-            padding:10px;
-        }
-        
-        .adImage{
-            width: 100%;
-        }
-
-       
-    }
-
-    .rightSide{
-        width:auto;
-        margin-top:20px;
-        .box{
-            flex-direction: column;
-            width:320px;
-            margin:auto;
-        }
-
-        .contactSellerLink{
-            width:320px;
-            margin: 20px auto;
-        }
-    }
-}
-`;
-
-export const OthersArea = styled.div`
-h2 {
-    font-size:20px;
-}
-.list {
-    display:flex;
-    flex-wrap: wrap;
-
-    .aditem {
-        width:25%;
-    }
-}
-
-@media(max-width:600px){
-    & {
-        margin:10px;
-    align-items: center;
-    display: flex;
-    flex-direction: column;
-    }
-
-    .list{
-        justify-content: center;
-        .aditem{
-            width: 50%;
-        }
-        
-    } 
-}
-`;
-
 export const BreadChumb = styled.div`
 font-size:13px;
 margin-top:20px;
@@ -175,6 +20,8 @@ a{
 `;
 
 export const Content = styled.div`
+    margin-top: 1rem;
+
     display: flex;
     padding: 0 1rem;
 .content--left{
@@ -271,8 +118,8 @@ export const Content = styled.div`
     }
 
     &.chat{
-        gap: 7px;
-        display: inline-flex;
+    gap: 7px;
+    display: inline-flex;
     -webkit-box-align: center;
     align-items: center;
     -webkit-box-pack: center;
@@ -293,8 +140,29 @@ export const Content = styled.div`
     }
     }
     }
-
+    small{
+        padding: 0 1rem;
+    }
         
+    }
+}
+
+@media screen and (max-width:762px){
+    flex-direction: column;
+
+    .content--left{
+        width: 100%;
+        max-width:100%;
+    }
+
+    .content--right{
+        display: flex;
+    align-items: center;
+    flex-direction: column;
+    justify-content: center;
+    gap: 2rem;
+    max-width:100%;
+    min-width:auto
     }
 }
 `
